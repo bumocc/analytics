c6ea3ccaa1dbcc358be7f80832073af623013926
bumocc <bumocc9@gmail.com>
2026-05-18 20:50:25 +0800

    refactor: enhance registration plug logic for clarity and maintainability

--- lib/plausible_web/plugs/maybe_disable_registration.ex
+++ lib/plausible_web/plugs/maybe_disable_registration.ex
@@ -1,11 +1,12 @@
 defmodule PlausibleWeb.Plugs.MaybeDisableRegistration do
   @moduledoc """
-  Plug toggling registration according to selfhosted state.
+  Plug toggling registration according to selfhosted state or auth plugin.
   """
 
   import Phoenix.Controller
   import Plug.Conn
 
+  alias Plausible.Auth.Plugin
   alias Plausible.Release
   alias PlausibleWeb.Router.Helpers, as: Routes
 
@@ -14,19 +15,30 @@ defmodule PlausibleWeb.Plugs.MaybeDisableRegistration do
   end
 
   def call(conn, _opts) do
+    cond do
+      Plugin.enabled?() ->
+        conn
+        |> redirect(external: Plugin.login_url())
+        |> halt()
+
+      registration_disabled?(conn) ->
+        conn
+        |> put_flash(:error, "Registration is disabled on this instance")
+        |> redirect(to: Routes.auth_path(conn, :login_form))
+        |> halt()
+
+      true ->
+        conn
+    end
+  end
+
+  defp registration_disabled?(conn) do
     disabled_for = List.wrap(conn.assigns.disable_registration_for)
 
     selfhost_config = Application.get_env(:plausible, :selfhost)
     disable_registration = Keyword.fetch!(selfhost_config, :disable_registration)
     first_launch? = Release.should_be_first_launch?()
 
-    if not first_launch? and disable_registration in disabled_for do
-      conn
-      |> put_flash(:error, "Registration is disabled on this instance")
-      |> redirect(to: Routes.auth_path(conn, :login_form))
-      |> halt()
-    else
-      conn
-    end
+    not first_launch? and disable_registration in disabled_for
   end
 end
